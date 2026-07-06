853805a63654f3042558a4f7637f9873cddef50e
Elvin Huseynov <codewithelvin@gmail.com>
2026-07-06 16:26:24 +0000

    Added responsivenese support

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,15 +1,20 @@
 import { Fragment } from "react";
 import stats from "../data/stats.json";
+import { BookIcon, CodeIcon } from "./Icons";
 
 export default function Statistics() {
   return (
-    <div className="flex flex-col sm:flex-row sm:items-center gap-2 mt-1">
-      <span className="text-sm text-muted text-gray-650">
-        Books: {stats.booksRead}
+    <div className="flex sm:flex-row sm:items-center gap-2 mt-1">
+      <span className="text-sm text-muted text-gray-650 flex items-center gap-1">
+        <BookIcon className="sm:hidden" />
+        <span className="hidden sm:inline">Books:</span>
+        <span>{stats.booksRead}</span>
       </span>
       <span className="text-sm text-muted text-gray-650">✦</span>
-      <span className="text-sm text-muted text-gray-650">
-        LeetCode: {stats.leetCode}
+      <span className="text-sm text-muted text-gray-650 flex items-center gap-1">
+        <CodeIcon className="sm:hidden" />
+        <span className="hidden sm:inline">LeetCode:</span>
+        <span>{stats.leetCode}</span>
       </span>
     </div>
   );
